2504b6fe047f8dfae9a874f99ae48e97f7bf1269
Kevin Holditch <kevholditch@hotmail.com>
2020-11-25 22:11:53 +0000

    starting to tidy up

--- internal/pkg/game/quad.go
+++ internal/pkg/game/quad.go
@@ -1,6 +1,10 @@
 package game
 
-import "github.com/go-gl/mathgl/mgl32"
+import (
+	"github.com/go-gl/gl/v2.1/gl"
+	"github.com/go-gl/mathgl/mgl32"
+	"github.com/kevholditch/breakout/internal/pkg/render"
+)
 
 /*
   X,W  ------- Z,W
@@ -38,4 +42,54 @@ func (q *Quad) ToBuffer() []float32 {
 		q.Position.Z(), q.Position.W(), q.Colour.X(), q.Colour.Y(), q.Colour.Z(), q.Colour.W(),
 		q.Position.X(), q.Position.W(), q.Colour.X(), q.Colour.Y(), q.Colour.Z(), q.Colour.W(),
 	}
+}
+
+func NewQuadShaderProgramOrPanic() *render.Program {
+	program, err := NewQuadShaderProgram()
+	if err != nil {
+		panic(err)
+	}
+	return program
+}
+
+func NewQuadShaderProgram() (*render.Program, error) {
+	vertex := `#version 410 core
+
+layout(location = 0) in vec4 position;
+layout(location = 1) in vec4 color;
+
+uniform mat4 u_MVP;
+
+out vec4 v_Color;
+
+void main()
+{
+	gl_Position = u_MVP * position;
+	v_Color = color;
+}`
+	vs, err := render.NewShaderFromString(vertex, gl.VERTEX_SHADER)
+	if err != nil {
+		return nil, err
+	}
+
+	fragment := `#version 410 core
+
+layout(location = 0) out vec4 o_Color;
+
+in vec4 v_Color;
+
+void main()
+{
+	o_Color = v_Color;
+}`
+	fs, err := render.NewShaderFromString(fragment, gl.FRAGMENT_SHADER)
+	if err != nil {
+		return nil, err
+	}
+
+	program, err := render.NewProgram(vs, fs)
+	if err != nil {
+		return nil, err
+	}
+	return program, nil
 }
--- internal/pkg/game/render_system.go
+++ internal/pkg/game/render_system.go
@@ -33,59 +33,11 @@ func NewRenderSystem(windowSize WindowSize) *RenderSystem {
 }
 
 func (r *RenderSystem) New(*ecs.World) {
-	err := r.initialise()
-	if err != nil {
-		panic(err)
-	}
-}
-
-func (r *RenderSystem) initialise() error {
 
 	render.UseDefaultBlending()
 	gl.ClearColor(colourDarkNavy.R, colourDarkNavy.G, colourDarkNavy.B, colourDarkNavy.A)
 
-	vertex := `#version 410 core
-
-layout(location = 0) in vec4 position;
-layout(location = 1) in vec4 color;
-
-uniform mat4 u_MVP;
-
-out vec4 v_Color;
-
-void main()
-{
-	gl_Position = u_MVP * position;
-	v_Color = color;
-}`
-	vs, err := render.NewShaderFromString(vertex, gl.VERTEX_SHADER)
-	if err != nil {
-		return err
-	}
-
-	fragment := `#version 410 core
-
-layout(location = 0) out vec4 o_Color;
-
-in vec4 v_Color;
-
-void main()
-{
-	o_Color = v_Color;
-}`
-	fs, err := render.NewShaderFromString(fragment, gl.FRAGMENT_SHADER)
-	if err != nil {
-		return err
-	}
-
-	program, err := render.NewProgram(vs, fs)
-	if err != nil {
-		return err
-	}
-
-	r.program = program
-
-	return nil
+	r.program = NewQuadShaderProgramOrPanic()
 }
 
 func (r *RenderSystem) generateVertexBuffer() []float32 {
